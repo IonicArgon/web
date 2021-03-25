02de18de21fef98298a238aff8642f1bb8102611
Marco Tan <marco.tan.200405@gmail.com>
2021-03-25 17:08:02 -0400

    temporary landing page

--- pages/index.js
+++ pages/index.js
@@ -5,61 +5,29 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Home - IonicArgon</title>
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+      <div class="max-w-2xl bg-white border-2 border-gray-300 p-5 rounded-md tracking-wide shadow-lg">
+        <div id="header" class="flex"> 
+          <img alt="mountain" class="w-60 h-60 rounded-md border-2 border-gray-300 object-cover" src="/temp.jpg"/>
+          <div id="body" class="flex flex-col ml-5">
+            <h4 id="name" class="text-2xl font-semibold mb-2">ionicargon.vercel.app</h4>
+            <h4 id="subtitle" class="text-xl text-gray-600 font-semibold mb-2">
+              This website is under construction.
+            </h4>
+            <p id="job" class="text-gray-800 mt-2">
+              I'm finally trying to learn web development.<br></br>
+              This website <i>will</i> be made with Next.js and Tailwind CSS.<br></br>
+              I look forward to showing you what I can do.
             </p>
-          </a>
+            <div class="flex mt-5">
+              <img alt="avatar" class="w-6 rounded-full border-2 border-gray-300" src="/profile-pic.png"/>
+              <p class="ml-3">Marco Tan</p>
+            </div>
+          </div>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      </div>
     </div>
   )
 }

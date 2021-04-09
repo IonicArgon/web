44226b3865087fa5f2be3a792ac806006b60317d
Marco Tan <marco.tan.200405@gmail.com>
2021-04-08 21:50:37 -0400

    nav done

---
+++ components/Layout.js
@@ -0,0 +1,36 @@
+import router, { useRouter } from 'next/router';
+import Nav from "./Nav";
+import styles from "../styles/Layout.module.css";
+
+// get meteorological seasons
+function getMeteoroSeason(){
+  let date = new Date();
+  if (date.getMonth() === 0 || (date.getMonth() < 2)) {
+    return 0; // winter = dec - feb
+  } else if (date.getMonth() >= 2 && date.getMonth() <= 4) {
+    return 1; // spring = mar - may
+  } else if (date.getMonth() >= 5 && date.getMonth() <= 7) {
+    return 2; // summer = jun - aug
+  } else {
+    return 3; // autumn = sept - nov
+  }
+}
+
+const Layout = ({ children }) => {
+  const Router = useRouter();
+  const showHeader = Router.pathname === "/" ? false : true;
+  const season = getMeteoroSeason();
+  
+  return (
+    <>
+      {showHeader && <Nav seasonID={season}></Nav>}
+      <div className={styles.container}>
+        <main className={styles.main}>
+          {children}
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default Layout;
---
+++ components/Nav.js
@@ -0,0 +1,77 @@
+import Link from "next/link";
+import { useState } from "react";
+
+// convert seasons to colours
+function seasonToColour(season) {
+  switch (season) {
+    case 0:
+      return "winter";
+    case 1:
+      return "spring";
+    case 2:
+      return "summer";
+    case 3:
+      return "autumn";
+    default:
+      return "black";
+  }
+}
+
+const Nav = ({ seasonID }) => {
+  let colour = seasonToColour(seasonID);
+
+  const [active, setActive] = useState(false);
+
+  const handleClick = () => {
+    setActive(!active);
+  }
+
+  return (
+    <>
+      <div id="navColor" className={"h-2 bg-" + colour}></div>
+      <nav className="flex items-center justify-between flex-wrap bg-greyd px-20 py-4">
+        <div className="flex items-center flex-shrink-0 mr-10">
+          <span className="text-2xl">
+            <text className="text-white">Ionic</text>
+            <text className={"text-" + colour}>Argon</text>
+          </span>
+        </div>
+        <div className="block md:hidden">
+          <button id="navButton" className={"flex items-center px-3 py-2 text-" + colour + " border-" + colour + " border rounded hover:text-white hover:border-white transition ease-in-out duration-300"} onClick={handleClick}>
+            <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/>
+              </svg>
+          </button>
+        </div>
+        <div id="navMenu" className={`${
+          active ? "" : "hidden"
+        } w-full block flex-grow md:flex md:items-center md:w-auto transform translate-y-1`}>
+          <div className="text-m md:flex-grow">
+            <Link href="/home">
+              <a href="#responsive-header" className={"block mt-4 md:inline-block md:mt-0 text-" + colour + " hover:text-white mr-4 transition ease-in-out duration-300"}>
+                Home
+              </a>
+            </Link>
+            <Link href="/about">
+              <a href="#responsive-header" className={"block mt-4 md:inline-block md:mt-0 text-" + colour + " hover:text-white mr-4 transition ease-in-out duration-300"}>
+                About
+              </a>
+            </Link>
+            <Link href="/portfolio">
+              <a href="#responsive-header" className={"block mt-4 md:inline-block md:mt-0 text-" + colour + " hover:text-white mr-4 transition ease-in-out duration-300"}>
+                Portfolio
+              </a>
+            </Link>
+            <Link href="/contact">
+              <a href="#responsive-header" className={"block mt-4 md:inline-block md:mt-0 text-" + colour + " hover:text-white mr-4 transition ease-in-out duration-300"}>
+                Contact
+              </a>
+            </Link>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Nav;

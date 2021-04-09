a04522adccd268711fabe7edf90851aabf986faf
Marco Tan <marco.tan.200405@gmail.com>
2021-04-08 21:51:33 -0400

    css changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
+// colour palette here: https://colorhunt.co/palette/69360
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        greyd: "#313131",
+        grey: "#414141",
+        greyl: "#525252",
+        winter: "#7daea3",
+        spring: "#a9b665",
+        summer: "#d8a657",
+        autumn: "#e78a4e",
+      }
+    },
   },
   variants: {
     extend: {},
